66194728e6cfbf04b94e927b24507f71f816f317
WIllianMartins2018 <willian.martins01@hotmail.com>
2023-06-28 20:34:56 -0300

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+	selector: 'app-root', // <app-root>
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+	public todos: Todo[] = [];
+	public title: String = 'Minhas Tarefas';
+	public form : FormGroup;
+
+
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			title : ['', Validators.compose([
+				Validators.minLength(3),
+				Validators.maxLength(60),
+				Validators.required
+			])]
+		});
+
+		this.todos.push(new Todo(1, 'Passear com o Cachorro', false));
+		this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+		this.todos.push(new Todo(3, 'Cortar Cabelo', true));
+	}
+
+	remove(todo: Todo) {
+		const index = this.todos.indexOf(todo);
+		if (index !== -1) {
+			this.todos.splice(index, 1);
+		}
+
+	}
+
+	markAsDone(todo: Todo) {
+		todo.done = true;
+	}
+
+	markAsUndone(todo: Todo) {
+		todo.done = false;
+	}
+
+	
+
+
+}
