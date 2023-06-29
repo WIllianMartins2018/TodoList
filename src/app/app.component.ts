d2904c9a1ab8510a576573b4eb3ff106f1fe9396
WIllianMartins2018 <willian.martins01@hotmail.com>
2023-06-29 20:41:57 -0300

    Salvando e Lendo Itens
    Salvando e Lendo Itens no localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,7 @@ export class AppComponent {
 				Validators.required
 			])]
 		});
-
-		this.todos.push(new Todo(1, 'Passear com o Cachorro', false));
-		this.todos.push(new Todo(2, 'Ir ao supermercado', false));
-		this.todos.push(new Todo(3, 'Cortar Cabelo', true));
+		this.load();
 	}
 
 	remove(todo: Todo) {
@@ -33,18 +30,42 @@ export class AppComponent {
 		if (index !== -1) {
 			this.todos.splice(index, 1);
 		}
+		this.save();
+	}
 
+	add() {
+		// this.form.value => {title: 'Titulo'}
+		const title = this.form.controls['title'].value;
+		const id = this.todos.length + 1;
+		this.todos.push(new Todo(id, title, false));
+		this.save();
+		this.clear();
+	}
+
+	clear() {
+		this.form.reset();
 	}
 
 	markAsDone(todo: Todo) {
 		todo.done = true;
+		this.save();
 	}
 
 	markAsUndone(todo: Todo) {
 		todo.done = false;
+		this.save();
+
 	}
 
-	
+	save() {
+		const data = JSON.stringify(this.todos);
+		localStorage.setItem('Todos', data);
+	}
 
+	load() {
+		const data = localStorage.getItem('Todos');
+		if (data)
+			this.todos = JSON.parse(data);
+	}
 
 }
